1f6955804b04ce28217659621663666738632ea1
Lara Liebold <s86444@stud.htw-dresden.de>
2026-01-07 15:48:32 +0100

    an review angepasst

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,16 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from database import Base, engine, SessionLocal
-from models import User
 from routers import user
 
 app = FastAPI()
-
-app.include_router(user.router)
-
-@app.get("/hello")
-def hello():
-    return {"message": "Hello from FastAPI!"}
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,27 +15,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(user.router)
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello from FastAPI!"}
 
 # Tabellen erstellen
 Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.post("/user")
-def create_user(name: str, db: Session = Depends(get_db)):
-    user = User(name=name)
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
-
-@app.get("/user")
-def list_user(db: Session = Depends(get_db)):
-    return db.query(user).all()
 
--- src/backend/routers/user.py
+++ src/backend/routers/user.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from database import SessionLocal
+from models import User
+
 
 router = APIRouter(
     prefix="/user",
@@ -9,3 +14,21 @@ router = APIRouter(
 def get_user():
     return ["example"]
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/")
+def create_user(name: str, db: Session = Depends(get_db)):
+    user = User(name=name)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+@router.get("/")
+def list_user(db: Session = Depends(get_db)):
+    return db.query(User).all()
