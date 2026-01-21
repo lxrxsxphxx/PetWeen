263afeca221826f1b4658dbb7b2338ba12b28859
Lara Liebold <s86444@stud.htw-dresden.de>
2026-01-21 11:16:28 +0100

    fertige tests für index page

--- src/petween/test/setup.ts
+++ src/petween/test/setup.ts
@@ -1,11 +1,18 @@
 import { config } from '@vue/test-utils'
+import { QToolbar } from 'quasar'
 
 // Stub Quasar components globally
     config.global.stubs = {
         Quasar: true,
-        QBtn: true,
+        QBtn: {
+            name: 'QBtn',
+            props: ['label'],
+            template: '<button>{{ label }}<slot /></button>'
+        },
         QIcon: true,
-        ScrollerCards: true,
-        PageHeader: true,
-        SectionHeader: true
+        QToolbar: true
+        //own components
+        //ScrollerCards: true,
+        //PageHeader: true,
+        //SectionHeader: true
     }
