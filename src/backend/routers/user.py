72e1d09fdd4a2263f4c71eb94dd11cfb619a275a
Lara Liebold <s86444@stud.htw-dresden.de>
2026-01-25 04:09:20 +0100

    tierische endpoints

--- src/backend/routers/user.py
+++ src/backend/routers/user.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, Depends, UploadFile, File
-from starlette.staticfiles import StaticFiles
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
 
 from sqlalchemy.orm import Session
 import os
 
-from backend.database import get_db, SessionLocal
+from backend.database import get_db
+
 from backend.models.user import User
 from backend.schemas.user import UserCreate, UserOut
 
+from backend.models.pet import Pet
 
 router = APIRouter(
     prefix="/user",
@@ -41,6 +42,41 @@ def create_user_with_image(
 
     return new_user
 
+
+@router.post("/{user_id}/pets/{pet_id}")
+def add_pet_to_user(
+    user_id: int,
+    pet_id: int,
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    pet = db.query(Pet).filter(Pet.id == pet_id).first()
+    if not pet:
+        raise HTTPException(status_code=404, detail="Pet not found")
+
+    if pet in user.pets:
+        raise HTTPException(status_code=400, detail="Pet already assigned to user")
+
+    user.pets.append(pet)
+    db.commit()
+
+    return {
+        "message": f"Pet '{pet.name}' added to user '{user.name}'"
+    }
+
+
 @router.get("/")
 def list_user(db: Session = Depends(get_db)):
     return db.query(User).all()
+
+@router.get("/users/{user_id}/pets")
+def get_pets_of_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user.pets
